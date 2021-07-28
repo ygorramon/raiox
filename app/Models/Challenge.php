0a4214e0289ad15d7346987139d8cd8f27c37921
ygorramon <ygor.ramon@ifce.edu.br>
2021-07-27 21:00:18 -0300

    Challenge Model

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -7,8 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Challenge extends Model
 {
     protected $fillable = [
-        'status','user_id','client_id'
+        'status','user_id','client_id','passo1','passo2',
+        'passo3_despertar','passo3_rotina_alimentar',
+        'passo3_rotina_sonecas','passo3_rotina_sonecas',
+        'passo3_ambiente_sonecas','passo3_ambiente_noturno',
+        'passo3_sono_noturno','passo4_associacoes_sonecas',
+        'passo4_associacoes_noturno','conclusao'
     ];
+
+
+    
     public function analyzes()
     {
         return $this->hasMany('App\Models\Analyze');
--- database/migrations/2021_07_10_020022_create_challenges_table.php
+++ database/migrations/2021_07_10_020022_create_challenges_table.php
@@ -18,6 +18,17 @@ class CreateChallengesTable extends Migration
             $table->string('status');
             $table->unsignedBigInteger('client_id');
             $table->unsignedBigInteger('user_id')->nullable();
+            $table->text('passo1')->nullable();
+            $table->text('passo2')->nullable();
+            $table->text('passo3_despertar')->nullable();
+            $table->text('passo3_rotina_alimentar')->nullable();
+            $table->text('passo3_rotina_sonecas')->nullable();
+            $table->text('passo3_ambiente_sonecas')->nullable();
+            $table->text('passo3_ambiente_noturno')->nullable();
+            $table->text('passo3_sono_noturno')->nullable();
+            $table->text('passo4_associacoes_sonecas')->nullable();
+            $table->text('passo4_associacoes_noturno')->nullable();
+            $table->text('conclusao')->nullable();
             $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 

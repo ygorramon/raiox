ef7ea05922ea262ca7966a9d632ec732c682e606
ygorramon <ygor.ramon@ifce.edu.br>
2021-08-24 15:07:21 -0300

    Command commit

---
+++ app/Console/Commands/UpdateChallenge.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Challenge;
+use App\User;
+use Illuminate\Console\Command;
+
+class UpdateChallenge extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:challenge';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Finaliza Desafios com mais de 15 dias';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $headers = ['Desafio', 'Enviado'];
+
+       Challenge::where('answered_at','<',now()->subDays(15))->update(['status' => 'FINALIZADO']);
+        
+    }
+}

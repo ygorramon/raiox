db43dbe7a4b83363862c6d9293580e786f415424
ygorramon <ygor.ramon@ifce.edu.br>
2021-09-01 16:01:53 -0300

    Commit

--- resources/views/site/desafio/show.blade.php
+++ resources/views/site/desafio/show.blade.php
@@ -157,108 +157,6 @@
                   </div>
                   
                   @endif
-                  @if($challenge->status=='FINALIZADO')
-                  <div class="row">
-                    <div class="col s12">
-                      <h4 class="card-title">Análise do Seu Desafio (Clique em cada Passo abaixo) </h4>
-                    </div>
-
-                    <ul class="collapsible">
-                      <li>
-                        <div class="collapsible-header"><i class="material-icons">place</i>Passo 1</div>
-                        <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo1}}</textarea>
-                        </div>
-                      </li>
-                      <li>
-                        <div class="collapsible-header"><i class="material-icons">place</i>Passo 2</div>
-                        <div class="collapsible-body"><textarea class="materialize-textarea" disabled> {{$challenge->passo2}}</textarea></div>
-                      </li>
-                      <li>
-                        <div class="collapsible-header"><i class="material-icons">place</i>Passo 3</div>
-                        <div class="collapsible-body">
-                          <ul class="collapsible">
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Despertar</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo3_despertar}}</textarea>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Rotina Alimentar</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo3_rotina_alimentar}}</textarea>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Rotina de Sonecas</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo3_rotina_sonecas}}</textarea>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Ambiente de Sonecas</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo3_ambiente_sonecas}}</textarea>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Sono Noturno</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo3_sono_noturno}}</textarea>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Ambiente do Sono Noturno</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo3_ambiente_noturno}}</textarea>
-                              </div>
-                            </li>
-                        </div>
-
-                      </li>
-                      <li>
-                        <div class="collapsible-header"><i class="material-icons">place</i>Passo 4</div>
-                        <div class="collapsible-body">
-                          <ul class="collapsible">
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Associações da Soneca</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo4_associacoes_sonecas}}</textarea>
-                              </div>
-                            </li>
-                            <li>
-                              <div class="collapsible-header"><i class="material-icons">place</i>Associações do Sono Noturno</div>
-                              <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->passo4_associacoes_noturno}}</textarea>
-                              </div>
-                            </li>
-                          </ul>
-                      <li>
-                        <div class="collapsible-header"><i class="material-icons">place</i>Conclusão</div>
-                        <div class="collapsible-body"> <textarea class="materialize-textarea" disabled> {{$challenge->conclusao}}</textarea>
-                        </div>
-                      </li>
-                    </ul>
-                    <ul class="collapsible">
-                      <li>
-                        <div class="collapsible-header"><i class="material-icons">message</i>Chat</div>
-                        <div class="collapsible-body">
-                          
-                         
-                          @foreach($challenge->chat()->first()->messages as $message)
-                          @if($message->type==1)
-                          <label >Eu:</label>
-                          <textarea class="materialize-textarea" readonly> {{$message->content}}</textarea>
-                          @endif
-                          @if($message->type==2)
-                          <label >Dr. Odilo:</label>
-                          <textarea class="materialize-textarea" readonly> {{$message->content}}</textarea>
-                          @endif
-                          @endforeach
-                         
-
-                         
-                          
-                        </div>
-                      </li>
-
-                    </ul>
-
-                  </div>
-                  
-                  @endif
                   @if($challenge->status=='INICIADO')
                   <h4 class="card-title">Análise Diária</h4>
 
@@ -276,7 +174,6 @@
                         <tbody>
                           <tr>
                             @if(!isset($challenge->analyzes()->where('day','1')->first()->day))
-
                             <td><a href="{{route('analyze.create',[$challenge->id,1])}}" class="btn waves-effect waves-light red "> Dia 1 </a>
                     </div>
                     @else
@@ -293,39 +190,38 @@
 
                   @endif
                   </tr>
+                  
                   @if(isset($challenge->analyzes()->where('day','1')->first()->day))
-                  <tr>
-                    @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','1')->first()->created_at->addDays(1),'Y-d-m'))
+              <tr>
+                @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','1')->first()->created_at->addDays(1),'Y-m-d'))
+                @if(!isset($challenge->analyzes()->where('day','2')->first()->day))
 
-                    @if(!isset($challenge->analyzes()->where('day','2')->first()->day))
+                <td><a href="{{route('analyze.create',[$challenge->id,2])}}" class="btn waves-effect waves-light red "> Dia 2 </a>
+            </div>
+            @else
+            <td><a href="{{route('analyze.edit',[$challenge->id,2])}}" class="btn waves-effect waves-light red "> Dia 2 </a>
+          </div>
+          @endif
 
-                    <td><a href="{{route('analyze.create',[$challenge->id,2])}}" class="btn waves-effect waves-light red "> Dia 2 </a>
-                </div>
-                @else
-                <td><a href="{{route('analyze.edit',[$challenge->id,2])}}" class="btn waves-effect waves-light red "> Dia 2 </a>
-              </div>
-              @endif
+        
+          @if(isset($challenge->analyzes()->where('day','2')->first()->day))
+          <td> <i class="material-icons">check</i> {{date_format($challenge->analyzes()->where('day','2')->first()->created_at,'d/m/Y') }} </td>
+          @else
+          <td>NÃO</td>
 
+          @endif
+          @else
+          <td>Dia 2</td>
+          <td>Disponível em {{date_format($challenge->analyzes()->where('day','1')->first()->created_at->addDays(1),'d/m/Y')}}</td>
 
+          @endif
+          </tr>
+          @endif
 
-              @if(isset($challenge->analyzes()->where('day','2')->first()->day))
-              <td> <i class="material-icons">check</i> {{date_format($challenge->analyzes()->where('day','2')->first()->created_at,'d/m/Y') }} </td>
-
-              @else
-              <td>NÃO</td>
-
-              @endif
-              @else
-              <td>Dia 2</td>
-              <td>Disponível em {{date_format($challenge->analyzes()->where('day','1')->first()->created_at->addDays(1),'d/m/Y')}}</td>
-
-              @endif
-              </tr>
-              @endif
 
               @if(isset($challenge->analyzes()->where('day','2')->first()->day))
               <tr>
-                @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','2')->first()->created_at->addDays(1),'Y-d-m'))
+                @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','2')->first()->created_at->addDays(1),'Y-m-d'))
                 @if(!isset($challenge->analyzes()->where('day','3')->first()->day))
 
                 <td><a href="{{route('analyze.create',[$challenge->id,3])}}" class="btn waves-effect waves-light red "> Dia 3 </a>
@@ -352,7 +248,7 @@
 
           @if(isset($challenge->analyzes()->where('day','3')->first()->day))
           <tr>
-            @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','3')->first()->created_at->addDays(1),'Y-d-m'))
+            @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','3')->first()->created_at->addDays(1),'Y-m-d'))
             @if(!isset($challenge->analyzes()->where('day','4')->first()->day))
 
             <td><a href="{{route('analyze.create',[$challenge->id,4])}}" class="btn waves-effect waves-light red "> Dia 4 </a>
@@ -380,7 +276,7 @@
 
       @if(isset($challenge->analyzes()->where('day','4')->first()->day))
       <tr>
-        @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','4')->first()->created_at->addDays(1),'Y-d-m'))
+        @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','4')->first()->created_at->addDays(1),'Y-m-d'))
 
         @if(!isset($challenge->analyzes()->where('day','5')->first()->day))
 
@@ -410,7 +306,7 @@
 
   @if(isset($challenge->analyzes()->where('day','5')->first()->day))
   <tr>
-    @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','5')->first()->created_at->addDays(1),'Y-d-m'))
+    @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','5')->first()->created_at->addDays(1),'Y-m-d'))
     @if(!isset($challenge->analyzes()->where('day','6')->first()->day))
 
     <td><a href="{{route('analyze.create',[$challenge->id,6])}}" class="btn waves-effect waves-light red "> Dia 6 </a>
@@ -434,7 +330,7 @@
 
 @if(isset($challenge->analyzes()->where('day','6')->first()->day))
 <tr>
-  @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','6')->first()->created_at->addDays(1),'Y-d-m'))
+  @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','6')->first()->created_at->addDays(1),'Y-m-d'))
 
   @if(!isset($challenge->analyzes()->where('day','7')->first()->day))
 
@@ -459,7 +355,7 @@
 
 @if(isset($challenge->analyzes()->where('day','7')->first()->day))
 <tr>
-  @if(date_format(now(),'Y-d-m')>=date_format($challenge->analyzes()->where('day','7')->first()->created_at->addDays(1),'Y-d-m'))
+  @if(date_format(now(),'Y-m-d')>=date_format($challenge->analyzes()->where('day','7')->first()->created_at->addDays(1),'Y-m-d'))
 
   @if(!isset($challenge->form()->first()->id))
 

163eb88736d79db2f494968c4d3399094fca360f
ygorramon <ygor.ramon@ifce.edu.br>
2021-11-12 09:06:32 -0300

    Commit

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -110,5 +110,5 @@ return $data;
 
 function diffDate($date_start, $date_end){
 return  \Carbon\Carbon::parse($date_end)
-->diffInDays(\Carbon\Carbon::parse($date_start));
+->diffInHours(\Carbon\Carbon::parse($date_start));
 }
--- resources/views/admin/relatorios/chats.blade.php
+++ resources/views/admin/relatorios/chats.blade.php
@@ -22,43 +22,48 @@
                     <th>Última mensagem</th>
                     <th>Horário da última mensagem</th>
                   
-                    <th>Diferença em Dias</th>
+                    <th>Diferença em Horas</th>
 
                   
                 </tr>
             </thead>
             <tbody>
                 @foreach ($chats as $chat)
+                @if($chat->challenge->status=="RESPONDIDO")
+                @if($chat->status=='mae')
                 <tr>
                 <td>{{ $chat->challenge->client->name }}</td>
-                <td> @if($chat->challenge->status=='ANALISE')
-                        <span class="badge bg-yellow">
-                            {{ $chat->$challenge->status }}</span>
-                            @endif
-                            @if($chat->challenge->status=='ENVIADO')
-                        <span class="badge bg-yellow">
-                            {{ $chat->challenge->status }}</span>
-                            @endif
+                <td> 
                             @if($chat->challenge->status=='RESPONDIDO')
                         <span class="badge bg-green">
                             {{ $chat->challenge->status }} 
 </span>
 
                             @endif
-                            @if($chat->challenge->status=='FINALIZADO')
-                        <span class="badge bg-blue">
-                            {{ $chat->challenge->status }} 
-</span>
-
-                            @endif
+                         
+                           
                     <td>{{$chat->challenge->user->name ?? ''}}</td>
                     <td>@if($chat->status=='mae')  <span class="badge bg-yellow"> CLIENTE </span> @else  <span class="badge bg-green"> TERAPEUTA </span>@endif</td>
                     <td> {{formatDateAndTimeHours($chat->messages->last()->created_at)}}</td>
-                    <td>@if($chat->status=='mae') {{diffDate($chat->messages->last()->created_at,now())}} 
+                    <td>@if($chat->status=='mae')
+                        @if(diffDate($chat->messages->last()->created_at,now()) < 24 )
+                        <span class="badge bg-green">  {{diffDate($chat->messages->last()->created_at,now())}} </span>
+                        @endif
+
+                        @if(diffDate($chat->messages->last()->created_at,now()) >= 24 && diffDate($chat->messages->last()->created_at,now()) < 48)
+                        <span class="badge bg-yellow">  {{diffDate($chat->messages->last()->created_at,now())}} </span>
+                        @endif
+
+                        @if(diffDate($chat->messages->last()->created_at,now()) >=48)
+                        <span class="badge bg-red">  {{diffDate($chat->messages->last()->created_at,now())}} </span>
+                        @endif
+
                         @else
                         {{diffDate($chat->messages->where('type','1')->last()->created_at, $chat->messages->last()->created_at)}}
                         @endif</td>
                 </tr>
+                @endif
+                @endif
                 @endforeach
             </tbody>
         </table>

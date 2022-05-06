d5ce0b4f06cc79e96e449fb82333f5550ffaac0a
ygorramon <ygor.ramon@ifce.edu.br>
2022-05-06 16:39:51 -0300

    Commit

--- app/Http/Controllers/Admin/ClientsController.php
+++ app/Http/Controllers/Admin/ClientsController.php
@@ -130,12 +130,13 @@ class ClientsController extends Controller
     }
 
     public function apiUser(Request $request){
-        if($request->hottok==env('HOTTOKEN')){
+       
         $client = $this->repository->create([
             'email' => $request->email,
+            'class' => $request->prod_name,
             'active' => 1,
             'expireAt' => now()->addDays(180),
         ]);
-    }
+    
     }
 }

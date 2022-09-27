accfc2c1966f9ed4c667443502c873914087e433
ygorramon <ygor.ramon@ifce.edu.br>
2022-09-27 19:49:16 -0300

    'commit'

--- app/Http/Controllers/Site/ChallengeController.php
+++ app/Http/Controllers/Site/ChallengeController.php
@@ -1781,17 +1781,16 @@ Faça o seu melhor, mas caso tenha dificuldades com as sonecas, conversaremos co
         $janelas = [];
 
         foreach ($challenge->naps as $nap) {
-            if (($nap->window - $nap->windowSignalSlept) < getSinalSono(getIdade($client->birthBaby))->janelaIdealFim) {
+            if ((($nap->window - $nap->windowSignalSlept) < getSinalSono(getIdade($client->birthBaby))->janelaIdealFim) && (($nap->window - $nap->windowSignalSlept) >= getSinalSono(getIdade($client->birthBaby))->janelaIdealInicio)) {
                 array_push($janelas, $nap->window - $nap->windowSignalSlept);
             }
         }
 
         foreach ($challenge->rituals as $ritual) {
-            if ($ritual->window < getSinalSono(getIdade($client->birthBaby))->janelaIdealFim) {
+            if ($ritual->window < getSinalSono(getIdade($client->birthBaby))->janelaIdealFim && $ritual->window >= getSinalSono(getIdade($client->birthBaby))->janelaIdealInicio) {
                 array_push($janelas, $ritual->window);
             }
         }
-
         if (count($janelas) > 0) {
             $media_janelas = array_sum($janelas) / count($janelas);
         } else {

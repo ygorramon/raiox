e6d53b3aa3a37e14879ad93838762e50fb6afb3d
ygorramon <ygor.ramon@ifce.edu.br>
2022-09-27 19:46:19 -0300

    'commit'

--- app/Http/Controllers/Site/ChallengeController.php
+++ app/Http/Controllers/Site/ChallengeController.php
@@ -1799,8 +1799,7 @@ Faça o seu melhor, mas caso tenha dificuldades com as sonecas, conversaremos co
         }
 
 
-        return view('site.desafio.passo3_rotina_sonecas', compact('challenge', 'client', 'janelas', '
-        ', 'qtd_sonecas_inadequadas'));
+        return view('site.desafio.passo3_rotina_sonecas', compact('challenge', 'client', 'janelas', 'media_janelas', 'qtd_sonecas_inadequadas'));
     }
     public function passo3_rotina_sonecas2($id)
     {
@@ -1843,14 +1842,14 @@ dd($janelas);
 
         $janelas = [];
 
-        foreach ($challenge->naps as $nap) {
-            if ((($nap->window - $nap->windowSignalSlept) < getSinalSono(getIdade($client->birthBaby))->janelaIdealFim) && (($nap->window - $nap->windowSignalSlept) >= getSinalSono(getIdade($client->birthBaby))->janelaIdealInicio)) {
+        foreach ($challenge->analyzes->where('day', 3)->first()->naps as $nap) {
+            if (($nap->window - $nap->windowSignalSlept) > getSinalSono(getIdade($client->birthBaby))->janelaIdealFim) {
                 array_push($janelas, $nap->window - $nap->windowSignalSlept);
             }
         }
 
-        foreach ($challenge->rituals as $ritual) {
-            if ($ritual->window < getSinalSono(getIdade($client->birthBaby))->janelaIdealFim && $ritual->window >= getSinalSono(getIdade($client->birthBaby))->janelaIdealInicio) {
+        foreach ($challenge->analyzes->where('day', 3)->first()->rituals as $ritual) {
+            if ($ritual->window > getSinalSono(getIdade($client->birthBaby))->janelaIdealFim) {
                 array_push($janelas, $ritual->window);
             }
         }

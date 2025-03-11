5e5eadb120a7d08853c381dd0c4ed78e6b595175
Ygor <ygor.ramon@ifce.edu.br>
2025-03-11 09:43:26 -0300

    commit

--- resources/views/admin/relatorios/enviados.blade.php
+++ resources/views/admin/relatorios/enviados.blade.php
@@ -19,6 +19,8 @@
             <thead>
                 <tr>
                     <th>Nome da Mãe</th>
+                    <th>Bebê</th>
+                    <th>Data de Nascimento - Idade</th>
                       <th>Email</th>
                     <th>Turma</th>
                     <th>Data de Envio</th>
@@ -34,45 +36,50 @@
             </thead>
             <tbody>
                 @foreach ($challenges as $challenge)
-                <tr>
-                    <td>{{ $challenge->client->name }}</td>
-                    <td>{{ $challenge->client->email }}</td>
-                    <td>{{ $challenge->client->class }}</td>
-                  <td> {{formatDateAndTimeHours($challenge->sended_at)}}</td>
-                    <td>{{ $challenge->user->name ?? ''}}</td>
+                                    <tr>
+                                        <td>{{ $challenge->client->name }}</td>
+                                        <td>{{ $challenge->client->nameBaby }}</td>
+                                        @if(!$challenge->client->birthBaby == Null) {{ 
+                                             \Carbon\Carbon::parse($challenge->client->birthBaby)->format('d/m/Y')
+                                             }}
+                                        @endif
+                                        <td>{{ $challenge->client->email }}</td>
+                                        <td>{{ $challenge->client->class }}</td>
+                                      <td> {{formatDateAndTimeHours($challenge->sended_at)}}</td>
+                                        <td>{{ $challenge->user->name ?? ''}}</td>
 
-                    <td> @if($challenge->status=='ANALISE')
-                        <span class="badge bg-yellow">
-                            {{ $challenge->status }}</span>
-                            @endif
-                            @if($challenge->status=='ENVIADO')
-                        <span class="badge bg-yellow">
-                            {{ $challenge->status }}</span>
-                            @endif
-                            @if($challenge->status=='RESPONDIDO')
-                        <span class="badge bg-green">
-                            {{ $challenge->status }} 
-</span>
+                                        <td> @if($challenge->status == 'ANALISE')
+                                            <span class="badge bg-yellow">
+                                                {{ $challenge->status }}</span>
+                                                @endif
+                                                @if($challenge->status == 'ENVIADO')
+                                            <span class="badge bg-yellow">
+                                                {{ $challenge->status }}</span>
+                                                @endif
+                                                @if($challenge->status == 'RESPONDIDO')
+                                            <span class="badge bg-green">
+                                                {{ $challenge->status }} 
+                    </span>
 
-                            @endif
-                            @if($challenge->status=='FINALIZADO')
-                        <span class="badge bg-blue">
-                            {{ $challenge->status }} 
-</span>
+                                                @endif
+                                                @if($challenge->status == 'FINALIZADO')
+                                            <span class="badge bg-blue">
+                                                {{ $challenge->status }} 
+                    </span>
 
-                            @endif
-                    </td>
-                   @if($challenge->status!='INICIADO')
-                    <td><a class="btn btn-primary" href="{{route('challenge.meus.show', $challenge->id)}}"> Ver Desafio</a>
-                    <a class="btn btn-warning" href="{{route('challenge.meus.respostas', $challenge->id)}}"> Ver Respostas</a>
-                    @if(isset($challenge->chat))
-                     <a class="btn btn-success" href="{{route('challenge.meus.chat', $challenge->id)}}"> Ver Chat</a>
-                        @endif
-                    </td>
-                    @endif
+                                                @endif
+                                        </td>
+                                       @if($challenge->status != 'INICIADO')
+                                        <td><a class="btn btn-primary" href="{{route('challenge.meus.show', $challenge->id)}}"> Ver Desafio</a>
+                                        <a class="btn btn-warning" href="{{route('challenge.meus.respostas', $challenge->id)}}"> Ver Respostas</a>
+                                        @if(isset($challenge->chat))
+                                         <a class="btn btn-success" href="{{route('challenge.meus.chat', $challenge->id)}}"> Ver Chat</a>
+                                            @endif
+                                        </td>
+                                        @endif
 
-                   </td>
-                </tr>
+                                       </td>
+                                    </tr>
                 @endforeach
             </tbody>
         </table>

552aa2dc337a06f3ebd3211a129900a6db5e04ac
Ygor <ygor.ramon@ifce.edu.br>
2025-09-03 10:43:34 -0300

    commit

--- resources/views/admin/challenges/availables/new-show.blade.php
+++ resources/views/admin/challenges/availables/new-show.blade.php
@@ -81,10 +81,10 @@
 @if(count($challenge->client->challenges->where('status', 'FINALIZADO')) > 1)
 
     @foreach($challenge->client->challenges->where('status', 'FINALIZADO') as $challenge2)
-        {{ $challenge2->anotacoes }}
-            @if($challenge2->chat != null)
-                <textarea class="form-control" style="height:auto">{{ $challenge2->chat->first()->messages->first()->content ?? '' }}</textarea>
-            @endif
+
+        @if($challenge2->chat != null)
+            <textarea class="form-control" style="height:auto"> {{ $challenge2->anotacoes }} <br><br>{{ $challenge2->chat->first()->messages->first()->content ?? '' }}</textarea>
+        @endif
     @endforeach
 
 @endif
